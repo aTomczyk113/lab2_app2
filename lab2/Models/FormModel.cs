ec189bc4e214a9e6c58450dac981e5a94f006bd0
akowa <akowa@DESKTOP-FF0QA95>
2021-12-31 02:23:33 +0100

    Add project files.

---
+++ lab2/Models/FormModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class FormModel
+    {
+        [Display(Name = "Imie")]
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Data")]
+        public DateTime Description { get; set; }
+      
+    }
+}
